fb0b9ed6d453751b0d96d64947d7045fd80ead71
Guilherme Gonzalez <gonzalez.etec@hotmail.com>
2019-03-20 03:24:17 +0000

    script para instalacao

---
+++ script.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+os.system("sudo apt-get install openvpn")
+os.system("wget acandreani.info/client_vlab.ovpn")
+os.system("sudo openvpn client_vlab.ovpn")
+os.system("sudo apt-get install apache2")
+
+
+
